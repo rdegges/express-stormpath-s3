b27b981209dd981cb6040894c5709447063f649a
Randall Degges <r@rdegges.com>
2016-09-13 15:06:29 -0700

    Adding tests for our constants module.

---
+++ test/test-constants.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const constants = require('../lib/constants');
+
+describe('constants', () => {
+  it('should expose a DEFAULT_ACL setting', () => {
+    assert(typeof constants.DEFAULT_ACL === 'string');
+  });
+
+  it('should expose a AWS_S3_BASE_URL setting', () => {
+    assert(typeof constants.AWS_S3_BASE_URL === 'string');
+  });
+});
