5ea0926190ab8b13b56a02faa9ec0f9b17775322
Randall Degges <r@rdegges.com>
2016-09-13 16:50:26 -0700

    Adding test for middleware.

--- test/test-index.js
+++ test/test-index.js
@@ -2,10 +2,28 @@
 
 const assert = require('assert');
 
+const express = require('express');
+
 const stormpathS3 = require('../index');
 
 describe('express-stormpath-s3', () => {
   it('should expose a middleware function', () => {
     assert(typeof stormpathS3 === 'function');
   });
+
+  it('should throw an error if the required options aren\'t specified', () => {
+    let app = express();
+
+    assert.throws(() => {
+      app.use(stormpathS3({}));
+    }, Error);
+  });
+
+  //it('should log a warning to the console if express-stormpath isn\'t initialized', (done) => {
+  //  let app = express();
+
+  //  app.use(stormpathS3({
+
+  //  }));
+  //});
 });
