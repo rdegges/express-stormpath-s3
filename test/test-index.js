f35e5ad85667b3348aa7c71b00cb50ca6cfbbc27
Randall Degges <r@rdegges.com>
2016-09-13 15:06:43 -0700

    Removing unnecessary dependency.

--- test/test-index.js
+++ test/test-index.js
@@ -2,9 +2,6 @@
 
 const assert = require('assert');
 
-const express = require('express');
-const stormpath = require('express-stormpath');
-
 const stormpathS3 = require('../index');
 
 describe('express-stormpath-s3', () => {
