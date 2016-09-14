b6e9b19fc3e261db257787d253281bac9da48d25
Randall Degges <r@rdegges.com>
2016-09-13 17:15:04 -0700

    Adding S3 bootstrapping for ITs.

--- test/test-index.js
+++ test/test-index.js
@@ -2,11 +2,44 @@
 
 const assert = require('assert');
 
+const AWS = require('aws-sdk');
 const express = require('express');
+const s3 = require('s3');
+const uuid = require('uuid4');
 
 const stormpathS3 = require('../index');
 
 describe('express-stormpath-s3', () => {
+  let bucket = uuid();
+  let s3Client;
+
+  before((done) => {
+    s3Client = new AWS.S3({
+      params: {
+        sslEnabled: true,
+        computeChecksums: true
+      }
+    });
+
+    s3Client.createBucket({ Bucket: bucket }, (err, data) => {
+      if (err) {
+        return done(err);
+      }
+
+      return done();
+    });
+  });
+
+  after((done) => {
+    s3Client.deleteBucket({ Bucket: bucket }, (err, data) => {
+      if (err) {
+        return done(err);
+      }
+
+      return done();
+    });
+  });
+
   it('should expose a middleware function', () => {
     assert(typeof stormpathS3 === 'function');
   });
@@ -19,11 +52,11 @@ describe('express-stormpath-s3', () => {
     }, Error);
   });
 
-  //it('should log a warning to the console if express-stormpath isn\'t initialized', (done) => {
-  //  let app = express();
+  it('should log a warning to the console if express-stormpath isn\'t initialized', (done) => {
+    let app = express();
 
-  //  app.use(stormpathS3({
+    app.use(stormpathS3({
 
-  //  }));
-  //});
+    }));
+  });
 });
