6b850c436e8b1fd9fac543938892fdaff0fdbea5
Randall Degges <r@rdegges.com>
2016-09-16 16:04:53 -0700

    Adding more thorough tests.

--- test/test-index.js
+++ test/test-index.js
@@ -10,10 +10,13 @@ const request = require('supertest');
 const sinon = require('sinon');
 const sp = require('stormpath');
 const stormpath = require('express-stormpath');
+const tmp = require('tmp');
 const uuid = require('uuid4');
 
 const stormpathS3 = require('../index');
 const utils = require('./utils');
+
+tmp.setGracefulCleanup();
 
 describe('express-stormpath-s3', () => {
   describe('exports', () => {
@@ -144,6 +147,170 @@ describe('express-stormpath-s3', () => {
               done();
             });
           });
+      });
+    });
+  });
+
+  describe('user methods', () => {
+    let bucket, spApplication, spClient;
+
+    beforeEach(done => {
+      spClient = new sp.Client();
+      this.sinon = sinon.sandbox.create();
+
+      async.parallel([
+        function(cb) {
+          utils.createS3Bucket((err, name) => {
+            if (err) {
+              return cb(err);
+            }
+
+            bucket = name;
+            cb(null);
+          });
+        },
+        function(cb) {
+          utils.createStormpathApplication(spClient, (err, app) => {
+            if (err) {
+              return cb(err);
+            }
+
+            spApplication = app;
+            cb();
+          });
+        }
+      ], err => {
+        if (err) {
+          return done(err);
+        }
+
+        done();
+      });
+    });
+
+    afterEach(done => {
+      this.sinon.restore();
+
+      async.parallel([
+        function(cb) {
+          utils.destroyStormpathApplication(spApplication, err => {
+            if (err) {
+              return cb(err);
+            }
+
+            cb();
+          });
+        },
+        function(cb) {
+          utils.destroyS3Bucket(bucket, err => {
+            if (err) {
+              return cb(err);
+            }
+
+            cb();
+          });
+        }
+      ], err => {
+        if (err) {
+          return done(err);
+        }
+
+        done();
+      });
+    });
+
+    describe('uploadFile', () => {
+      it('should return an error if the specified file does not exist', done => {
+        let agent;
+        let app = express();
+
+        app.use(stormpath.init(app, {
+          application: {
+            href: spApplication.href
+          }
+        }));
+        app.use(stormpath.getUser);
+        app.use(stormpathS3({ awsBucket: bucket }));
+
+        app.get('/', stormpath.loginRequired, (req, res) => {
+          req.user.uploadFile('file', err => {
+            assert(err);
+            res.send();
+          });
+        });
+
+        utils.createStormpathAccount(spApplication, (err, acc) => {
+          if (err) {
+            return done(err);
+          }
+
+          agent = request.agent(app);
+
+          agent.post('/login')
+            .type('form')
+            .send({ login: acc.username })
+            .send({ password: '0HIthere!0' })
+            .expect(302, (err, res) => {
+              if (err) {
+                return done(err);
+              }
+
+              agent.get('/').expect(200, (err, res) => {
+                if (err) {
+                  return done(err);
+                }
+
+                done();
+              });
+            });
+        });
+      });
+
+      it('should successfully upload files', done => {
+        let agent;
+        let app = express();
+        let tmpFile = tmp.fileSync();
+
+        app.use(stormpath.init(app, {
+          application: {
+            href: spApplication.href
+          }
+        }));
+        app.use(stormpath.getUser);
+        app.use(stormpathS3({ awsBucket: bucket }));
+
+        app.get('/', stormpath.loginRequired, (req, res) => {
+          req.user.uploadFile(tmpFile.name, err => {
+            assert(!err);
+            res.send();
+          });
+        });
+
+        utils.createStormpathAccount(spApplication, (err, acc) => {
+          if (err) {
+            return done(err);
+          }
+
+          agent = request.agent(app);
+
+          agent.post('/login')
+            .type('form')
+            .send({ login: acc.username })
+            .send({ password: '0HIthere!0' })
+            .expect(302, (err, res) => {
+              if (err) {
+                return done(err);
+              }
+
+              agent.get('/').expect(200, (err, res) => {
+                if (err) {
+                  return done(err);
+                }
+
+                done();
+              });
+            });
+        });
       });
     });
   });
