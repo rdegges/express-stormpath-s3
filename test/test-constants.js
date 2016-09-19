14b159321c555c912e18e3bba0469fe2aeb32189
Randall Degges <r@rdegges.com>
2016-09-19 15:09:20 -0700

    Style.

--- test/test-constants.js
+++ test/test-constants.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
+const assert = require("assert");
 
-const constants = require('../lib/constants');
+const constants = require("../lib/constants");
 
-describe('constants', () => {
-  it('should expose a DEFAULT_ACL setting', () => {
-    assert(typeof constants.DEFAULT_ACL === 'string');
+describe("constants", () => {
+  it("should expose a DEFAULT_ACL setting", () => {
+    assert(typeof constants.DEFAULT_ACL === "string");
   });
 
-  it('should expose a AWS_S3_BASE_URL setting', () => {
-    assert(typeof constants.AWS_S3_BASE_URL === 'string');
+  it("should expose a AWS_S3_BASE_URL setting", () => {
+    assert(typeof constants.AWS_S3_BASE_URL === "string");
   });
 });
